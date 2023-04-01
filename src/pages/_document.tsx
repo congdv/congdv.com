8ce3c6ca260c078ca6d71d01902c1d7df5026039
Cong Dao <congdaovan94@gmail.com>
2023-04-01 15:46:12 -0400

    Fix google analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,3 +1,4 @@
+import { GA_TRACKING_ID } from '@/scripts/gtag'
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
@@ -7,6 +8,23 @@ export default function Document() {
         <link
           href="https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,100;0,400;0,700;1,700&display=swap"
           rel="stylesheet"
+        />
+        {/* Global Site Tag (gtag.js) - Google Analytics */}
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+          }}
         />
       </Head>
       <body>
