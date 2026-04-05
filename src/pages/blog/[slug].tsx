cf07431798c8d50e2fa55fdcbe5c82a03b252004
Cong Dao <congdaovan94@gmail.com>
2026-04-05 19:36:37 -0400

    Refactor code structure for improved readability and maintainability

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -1,18 +1,18 @@
 import BlogPost from '@/components/BlogPost'
 import { getPost, getPosts } from '@/scripts/utils'
-import { MDXRemote } from 'next-mdx-remote'
-import { serialize } from 'next-mdx-remote/serialize'
+import { remark } from 'remark'
+import html from 'remark-html'
 
 export default function BlogPage({
   data,
-  content,
+  contentHtml,
 }: {
   data: any
-  content: any
+  contentHtml: string
 }) {
   return (
     <BlogPost meta={data}>
-      <MDXRemote {...content} />
+      <div dangerouslySetInnerHTML={{ __html: contentHtml }} />
     </BlogPost>
   )
 }
@@ -28,11 +28,15 @@ export const getStaticPaths = async () => {
 
 export const getStaticProps = async ({ params }: { params: any }) => {
   const post = await getPost(params.slug)
-  const mdxSource = await serialize(post.content)
+  const processedContent = await remark()
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    .use(html as any, { sanitize: false })
+    .process(post.content)
+  const contentHtml = processedContent.toString()
   return {
     props: {
       data: post.data,
-      content: mdxSource,
+      contentHtml,
     },
   }
 }
